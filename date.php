ab6295ef970462a4f71afea4a47b35c878066c24
Dmitrii Pezhemskii <dmpe0914@netcracker.com>
2017-09-30 11:29:45 +0300

    4

--- date.php
+++ date.php
@@ -3,7 +3,7 @@ date_default_timezone_set('Europe/Moscow');
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: text/html; charset=windows-1251;');
 $fp = file("filmbase");
-echo "Обновлен ".date("d/m/Y", filemtime('filmbase')). ". Фильмов: ". count($fp)
+echo "ќбновлен ".date("d/m/Y", filemtime('filmbase')). ". Ф‘ильмов: ". count($fp)
 #echo iconv("cp1251","UTF-8","ќбновлен " . date("d/m/Y", filemtime('filmbase')). ". ‘ильмов: ". count($fp));                                 
 
 ?>
